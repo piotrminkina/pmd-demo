b0c27e8a8231df77aa6554b7099f28edc0db7754
Piotr Minkina <p.minkina@piotrminkina.pl>
2014-07-14 20:52:49 +0200

    Initialized and mapped base model to use with PMDStateMachineBundle

---
+++ src/PMD/WorkflowDemoBundle/Model/Invoice.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Workflow Demo package.
+ *
+ * (c) Piotr Minkina <projekty@piotrminkina.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PMD\WorkflowDemoBundle\Model;
+
+/**
+ * Class Invoice
+ * 
+ * @author Piotr Minkina <projekty@piotrminkina.pl>
+ * @package PMD\WorkflowDemoBundle\Model
+ */
+class Invoice implements InvoiceInterface
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $state;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+}
